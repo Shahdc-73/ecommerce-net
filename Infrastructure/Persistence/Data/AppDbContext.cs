9b6b90ab1c85ea78a8b43de80ec34e7ac6723dd0
abdulrahman <abdulrahmn398@outlook.com>
2025-05-31 18:07:20 +0300

    Remove weather functionality and add EF Core support
    - Removed `WeatherForecastController.cs`.
    - Added `Microsoft.EntityFrameworkCore.Tools` package to `E-Commerce.csproj`.
    - Updated `Program.cs` to configure Entity Framework Core with SQL Server.
    - Added connection string for SQL Server in `appsettings.json`.
    - Included `Microsoft.EntityFrameworkCore.SqlServer` package in `Persistence.csproj`.
    - Created `AppDbContext.cs` for database context and entity sets.
    - Added `ProductConfiguration.cs` for configuring `Product` entity.
    - Created migration file `20250531150612_Initail.cs` for initial schema.
    - Generated `20250531150612_Initail.Designer.cs` for migration metadata.
    - Created `AppDbContextModelSnapshot.cs` for model state representation.

---
+++ Infrastructure/Persistence/Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+       : base(options)
+    {
+
+    }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<ProductBrand> ProductBrands { get; set; }
+    public DbSet<ProductType> ProductTypes { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+    }
+}
